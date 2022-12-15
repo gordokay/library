bedcdb8c10412294687b0f06d614bdb3acc0da07
Kayleigh <57508868+gordokay@users.noreply.github.com>
2022-12-15 18:36:20 -0500

    Add book creation functions

--- app.js
+++ app.js
@@ -6,8 +6,43 @@ const pageNumberInput = document.getElementById('page-number');
 const isFinishedInput = document.getElementById('is-finished');
 const addBookButton = document.querySelector('.add-book');
 const closeDialogButton = document.querySelector('.close-dialog');
+const libraryContainer = document.querySelector('.library');
 
-const library = document.querySelector('.library');
+const library = [];
+
+function Book(title, author, pageNumber, isFinished) {
+  this.title = title;
+  this.author = author;
+  this.pageNumber = pageNumber;
+  this.isFinished = isFinished;
+}
+
+Book.booksCreated = 0;
+
+function addBookToLibrary() {
+  const newBook = new Book(titleInput.value, authorInput.value, pageNumberInput.value, isFinishedInput.checked);
+  Book.booksCreated++;
+  newBook.id = Book.booksCreated;
+  library.push(newBook);
+  return newBook;
+}
+
+function makeBook(book) {
+  const newBook = document.createElement('div');
+  newBook.setAttribute('data-id', book.id);
+  const title = document.createElement('h3');
+  title.textContent = book.title;
+  const author = document.createElement('p');
+  author.textContent = book.author;
+  const pageNumber = document.createElement('p');
+  pageNumber.textContent = book.pageNumber;
+  const isFinished = document.createElement('input');
+  isFinished.setAttribute('type', 'checkbox');
+  isFinished.checked = book.isFinished;
+  isFinished.disabled = true;
+  newBook.append(title, author, pageNumber, isFinished);
+  libraryContainer.appendChild(newBook);
+}
 
 function clearForm() {
   [titleInput, authorInput, pageNumberInput].forEach(input => input.value = '');
@@ -24,5 +59,6 @@ closeDialogButton.addEventListener('click', () => {
 })
 
 form.addEventListener('submit', () => {
+  makeBook(addBookToLibrary());
   clearForm();
 })
